c172344ec4b7db6f5256aab568e26876e427d095
ray wang <wangrui@bongmi.com>
2017-11-30 18:54:28 +0800

    merge sort refinement: use insertion sort in small array, bottom up, check when array are already sorted

--- src/chap2/MergeSort.java
+++ src/chap2/MergeSort.java
@@ -27,6 +27,48 @@ public class MergeSort {
         }
     }
 
+    // 2.2.10 quick merge
+    public static void merge1(Comparable[] a, Comparable[] aux, int low, int
+            mid, int high) {
+        for (int i = low; i <= mid; i++) {
+            aux[i] = a[i];
+        }
+        for (int i = mid + 1; i <= high; i++) {
+            aux[i] = a[mid + 1 + high - i];
+        }
+        int i = low, j = high, k = low;
+        while (i <= j) {
+            if (Utils.less(aux[i], aux[j])) {
+                a[k++] = aux[i++];
+            } else {
+                a[k++] = aux[j--];
+            }
+        }
+        return;
+    }
+
+    // 2.2.11 swap destination and source alternatively, just merge without
+    // copy data
+    public static void merge2(Comparable[] des, Comparable[] src, int low, int
+            mid, int high) {
+//        if (mid + 1 > high || Utils.less(des[mid], des[mid + 1])) { //2.2.2.2 if is
+//            // already sorted, do not merge again
+//            return;
+//        }
+        int i = low, j = mid + 1;
+        for (int k = low; k <= high; k++) {
+            if (i > mid) {
+                des[k] = src[j++];
+            } else if (j > high) {
+                des[k] = src[i++];
+            } else if (Utils.less(src[i], src[j])) {
+                des[k] = src[i++];
+            } else {
+                des[k] = src[j++];
+            }
+        }
+    }
+
     public static void sortTD(Comparable[] a, Comparable[] aux, int low,
                               int high) {
         if (low >= high) {
@@ -38,7 +80,7 @@ public class MergeSort {
         int mid = low + (high - low) / 2;
         sortTD(a, aux, low, mid);
         sortTD(a, aux, mid + 1, high);
-        merge(a, aux, low, mid, high);
+        merge1(a, aux, low, mid, high);
     }
 
     // Top down
@@ -49,10 +91,20 @@ public class MergeSort {
 
     public static void sortBU(Comparable[] a, Comparable[] aux, int low, int
             high) {
+        boolean flag = false;
         for (int size = 1; size < a.length; size += size) {
             for (int i = low; i + size <= high; i += 2 * size) {
-                merge(a, aux, i, i + size - 1, Math.min(i + 2 * size -
+                merge2(aux, a, i, i + size - 1, Math.min(i + 2 * size -
                         1, high));
+            }
+            flag = !flag;
+            Comparable[] t = a;
+            a = aux;
+            aux = t;
+        }
+        if (flag) {
+            for (int i = low; i <= high; i++) {
+                a[i] = aux[i];
             }
         }
     }
